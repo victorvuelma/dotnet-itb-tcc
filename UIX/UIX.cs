2af62148c177740807bf6893a5e5bd06a1661a16
vitorrdgs <rodrigues.victor@outlook.com>
2018-04-15 16:52:29 -0300

    Atualização para uso de Fontes por estilo no UIX.

--- UIX/UIX.cs
+++ UIX/UIX.cs
@@ -15,47 +15,43 @@ namespace UIX
 
     public class UIXColor
     {
-        public static Color AQUA_LIGHT = UIX.UIXUtil.fromRGB(26, 188, 156);
-        public static Color AQUA_DARK = UIX.UIXUtil.fromRGB(22, 160, 133);
+        public static Color AQUA_LIGHT = UIXUtil.fromRGB(26, 188, 156);
+        public static Color AQUA_DARK = UIXUtil.fromRGB(22, 160, 133);
 
-        public static Color BLUE_LIGHT = UIX.UIXUtil.fromRGB(52, 152, 219);
-        public static Color BLUE_DARK = UIX.UIXUtil.fromRGB(41, 128, 185);
+        public static Color BLUE_LIGHT = UIXUtil.fromRGB(52, 152, 219);
+        public static Color BLUE_DARK = UIXUtil.fromRGB(41, 128, 185);
 
-        public static Color GREEN_LIGHT = UIX.UIXUtil.fromRGB(46, 204, 113);
-        public static Color GREEN_DARK = UIX.UIXUtil.fromRGB(39, 174, 96);
+        public static Color GREEN_LIGHT = UIXUtil.fromRGB(46, 204, 113);
+        public static Color GREEN_DARK = UIXUtil.fromRGB(39, 174, 96);
 
-        public static Color GRAY_LIGHT = UIX.UIXUtil.fromRGB(149, 165, 166);
-        public static Color GRAY_DARK = UIX.UIXUtil.fromRGB(127, 140, 141);
+        public static Color GRAY_LIGHT = UIXUtil.fromRGB(149, 165, 166);
+        public static Color GRAY_DARK = UIXUtil.fromRGB(127, 140, 141);
 
-        public static Color INDIGO_LIGHT = UIX.UIXUtil.fromRGB(52, 73, 94);
-        public static Color INDIGO_DARK = UIX.UIXUtil.fromRGB(44, 62, 80);
+        public static Color INDIGO_LIGHT = UIXUtil.fromRGB(52, 73, 94);
+        public static Color INDIGO_DARK = UIXUtil.fromRGB(44, 62, 80);
 
-        public static Color PURPLE_LIGHT = UIX.UIXUtil.fromRGB(155, 89, 182);
-        public static Color PURPLE_DARK = UIX.UIXUtil.fromRGB(142, 68, 173);
+        public static Color PURPLE_LIGHT = UIXUtil.fromRGB(155, 89, 182);
+        public static Color PURPLE_DARK = UIXUtil.fromRGB(142, 68, 173);
 
-        public static Color RED_LIGHT = UIX.UIXUtil.fromRGB(231, 76, 60);
-        public static Color RED_DARK = UIX.UIXUtil.fromRGB(192, 57, 43);
+        public static Color RED_LIGHT = UIXUtil.fromRGB(231, 76, 60);
+        public static Color RED_DARK = UIXUtil.fromRGB(192, 57, 43);
 
-        public static Color SILVER = UIX.UIXUtil.fromRGB(189, 195, 199);
-        public static Color WHITE = UIX.UIXUtil.fromRGB(236, 240, 241);
+        public static Color SILVER = UIXUtil.fromRGB(189, 195, 199);
+        public static Color WHITE = UIXUtil.fromRGB(236, 240, 241);
 
-        public static Color YELLOW_LIGHT = UIX.UIXUtil.fromRGB(241, 196, 15);
-        public static Color YELLOW_DARK = UIX.UIXUtil.fromRGB(243, 156, 18);
+        public static Color YELLOW_LIGHT = UIXUtil.fromRGB(241, 196, 15);
+        public static Color YELLOW_DARK = UIXUtil.fromRGB(243, 156, 18);
 
     }
 
     public class UIXFont
     {
 
-        public static Font LATO_BLACK(float size)
-        {
-            return new System.Drawing.Font("Lato Black", size, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-        }
+        public static Font CENTURY_REGULAR = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 
-        public static Font SANS_SERIF(float size)
-        {
-            return new System.Drawing.Font("Microsoft Sans Serif", size, System.Drawing.FontStyle.Bold);
-        }
+        public static Font LATO_BLACK = new System.Drawing.Font("Lato Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+
+        public static Font SANS_SERIF = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
 
         public static Font fontSize(Font font, float size)
         {
@@ -82,59 +78,72 @@ namespace UIX
         public Color DarkColor { get => _darkColor; }
         public Color LightColor { get => _lightColor; }
 
-        public static UIX.UIXSet AQUA = new UIX.UIXSet(UIX.UIXColor.AQUA_DARK, UIX.UIXColor.AQUA_LIGHT, UIX.UIXColor.WHITE);
-        public static UIX.UIXSet BLUE = new UIX.UIXSet(UIX.UIXColor.BLUE_DARK, UIX.UIXColor.BLUE_LIGHT, UIX.UIXColor.WHITE);
-        public static UIX.UIXSet GRAY = new UIX.UIXSet(UIX.UIXColor.GRAY_DARK, UIX.UIXColor.GRAY_LIGHT, UIX.UIXColor.INDIGO_LIGHT);
-        public static UIX.UIXSet GREEN = new UIX.UIXSet(UIX.UIXColor.GREEN_DARK, UIX.UIXColor.GREEN_LIGHT, UIX.UIXColor.WHITE);
-        public static UIX.UIXSet INDIGO = new UIX.UIXSet(UIX.UIXColor.INDIGO_DARK, UIX.UIXColor.INDIGO_LIGHT, UIX.UIXColor.WHITE);
-        public static UIX.UIXSet PURPLE = new UIX.UIXSet(UIX.UIXColor.PURPLE_DARK, UIX.UIXColor.PURPLE_LIGHT, UIX.UIXColor.WHITE);
-        public static UIX.UIXSet RED = new UIX.UIXSet(UIX.UIXColor.RED_DARK, UIX.UIXColor.RED_LIGHT, UIX.UIXColor.WHITE);
-        public static UIX.UIXSet WHITE = new UIX.UIXSet(UIX.UIXColor.SILVER, UIX.UIXColor.WHITE, UIX.UIXColor.INDIGO_LIGHT);
+        public static UIXSet AQUA = new UIXSet(UIXColor.AQUA_DARK, UIXColor.AQUA_LIGHT, UIXColor.WHITE);
+        public static UIXSet BLUE = new UIXSet(UIXColor.BLUE_DARK, UIXColor.BLUE_LIGHT, UIXColor.WHITE);
+        public static UIXSet GRAY = new UIXSet(UIXColor.GRAY_DARK, UIXColor.GRAY_LIGHT, UIXColor.INDIGO_LIGHT);
+        public static UIXSet GREEN = new UIXSet(UIXColor.GREEN_DARK, UIXColor.GREEN_LIGHT, UIXColor.WHITE);
+        public static UIXSet INDIGO = new UIXSet(UIXColor.INDIGO_DARK, UIXColor.INDIGO_LIGHT, UIXColor.WHITE);
+        public static UIXSet PURPLE = new UIXSet(UIXColor.PURPLE_DARK, UIXColor.PURPLE_LIGHT, UIXColor.WHITE);
+        public static UIXSet RED = new UIXSet(UIXColor.RED_DARK, UIXColor.RED_LIGHT, UIXColor.WHITE);
+        public static UIXSet WHITE = new UIXSet(UIXColor.SILVER, UIXColor.WHITE, UIXColor.INDIGO_LIGHT);
 
     }
 
     public class UIXStyle
     {
 
-        private UIX.UIXSet _formColor;
+        private UIXSet _formColor;
 
-        private UIX.UIXSet _headerButtonColor;
-        private UIX.UIXSet _headerCloseButtonColor;
+        private UIXSet _headerButtonColor;
+        private UIXSet _headerCloseButtonColor;
 
         private int _headerButtonSize;
         private int _headerButtonSpace;
 
-        private UIX.UIXSet _btnColor;
-        private UIX.UIXSet _grbColor;
-        private UIX.UIXSet _lblColor;
-        private UIX.UIXSet _txtColor;
+        private Font _headerFont;
+        private Font _contentFont;
 
-        public UIXStyle(UIX.UIXSet formColor, UIX.UIXSet headerButtonColor, UIX.UIXSet headerCloseButtonColor,
+        private UIXSet _btnColor;
+        private UIXSet _grbColor;
+        private UIXSet _lblColor;
+        private UIXSet _txtColor;
+
+        public UIXStyle(UIXSet formColor, UIXSet headerButtonColor, UIXSet headerCloseButtonColor,
             int headerButtonSize, int headerButtonSpace,
-            UIX.UIXSet btnColor, UIX.UIXSet grbColor, UIX.UIXSet lblColor, UIX.UIXSet txtColor)
+            Font headerFont, Font contentFont,
+            UIXSet btnColor, UIXSet grbColor, UIXSet lblColor, UIXSet txtColor)
         {
             _formColor = formColor;
+
             _headerButtonColor = headerButtonColor;
             _headerCloseButtonColor = headerCloseButtonColor;
+
+            _headerFont = headerFont;
+            _contentFont = contentFont;
+
             _headerButtonSize = headerButtonSize;
             _headerButtonSpace = headerButtonSpace;
+
             _btnColor = btnColor;
             _grbColor = grbColor;
             _lblColor = lblColor;
             _txtColor = txtColor;
         }
 
-        public UIX.UIXSet FormColor { get => _formColor; }
-        public UIX.UIXSet HeaderButtonColor { get => _headerButtonColor; }
-        public UIX.UIXSet HeaderCloseButtonColor { get => _headerCloseButtonColor; }
+        public UIXSet FormColor { get => _formColor; }
+        public UIXSet HeaderButtonColor { get => _headerButtonColor; }
+        public UIXSet HeaderCloseButtonColor { get => _headerCloseButtonColor; }
 
         public int HeaderButtonSize { get => _headerButtonSize; }
         public int HeaderButtonSpace { get => _headerButtonSpace; }
 
-        public UIX.UIXSet ButtonColor { get => _btnColor; }
-        public UIX.UIXSet GroupBoxColor { get => _grbColor; }
-        public UIX.UIXSet LabelColor { get => _lblColor; }
-        public UIX.UIXSet TextBoxColor { get => _txtColor; }
+        public Font HeaderFont { get => _headerFont; }
+        public Font ContentFont { get => _contentFont; }
+
+        public UIXSet ButtonColor { get => _btnColor; }
+        public UIXSet GroupBoxColor { get => _grbColor; }
+        public UIXSet LabelColor { get => _lblColor; }
+        public UIXSet TextBoxColor { get => _txtColor; }
 
         public static UIXStyle BLUE = new UIXStyle(
             UIXSet.INDIGO,
@@ -142,6 +151,8 @@ namespace UIX
             UIXSet.RED,
             30,
             5,
+                        UIXFont.LATO_BLACK,
+            UIXFont.CENTURY_REGULAR,
             UIXSet.BLUE,
             UIXSet.INDIGO,
             UIXSet.INDIGO,
@@ -154,7 +165,9 @@ namespace UIX
             UIXSet.RED,
             30,
             5,
-            UIXSet.WHITE,
+                                    UIXFont.LATO_BLACK,
+                        UIXFont.CENTURY_REGULAR,
+            UIXSet.GRAY,
             UIXSet.GRAY,
             UIXSet.GRAY,
             UIXSet.WHITE
@@ -182,8 +195,8 @@ namespace UIX
     public class UIXTemplate
     {
 
-        private UIX.UIXStyle _style;
-        private UIX.UIXImageTemplate _imageTemplate;
+        private UIXStyle _style;
+        private UIXImageTemplate _imageTemplate;
 
         public UIXTemplate(UIXStyle style, UIXImageTemplate imageTemplate)
         {
@@ -229,12 +242,12 @@ namespace UIX
 
         public void btnApply(Button btn)
         {
-            UIX.UIXButton.btnApply(btn, Style.ButtonColor);
+            UIXButton.btnApply(btn, Style.ButtonColor, Style.ContentFont);
         }
 
         public void grbApply(GroupBox grb)
         {
-            UIX.UIXGroupBox.grbApply(grb, Style.GroupBoxColor);
+            UIXGroupBox.grbApply(grb, Style.GroupBoxColor, Style.ContentFont);
 
             foreach (Control ctl in grb.Controls)
             {
@@ -244,12 +257,12 @@ namespace UIX
 
         public void lblApply(Label lbl)
         {
-            UIX.UIXLabel.lblApply(lbl, Style.LabelColor);
+            UIXLabel.lblApply(lbl, Style.LabelColor, Style.ContentFont);
         }
 
         public void pnlApply(Panel pnl)
         {
-            UIX.UIXPanel.pnlApply(pnl, Style.FormColor);
+            UIXPanel.pnlApply(pnl, Style.FormColor, Style.ContentFont);
 
             foreach (Control ctl in pnl.Controls)
             {
@@ -259,12 +272,12 @@ namespace UIX
 
         public void txtApply(TextBox txt)
         {
-            UIX.UIXTextBox.txtApply(txt, Style.TextBoxColor);
+            UIXTextBox.txtApply(txt, Style.TextBoxColor, Style.ContentFont);
         }
 
         public void uctApply(UserControl uct)
         {
-            UIX.UIXUserControl.uctApply(uct, Style.FormColor);
+            UIXUserControl.uctApply(uct, Style.FormColor, Style.ContentFont);
 
             foreach (Control ctl in uct.Controls)
             {
@@ -274,7 +287,7 @@ namespace UIX
 
         public void frmApply(Form frm, uctUIX ctl)
         {
-            UIX.UIXForm.frmApply(frm, ctl, Style);
+            UIXForm.frmApply(frm, ctl, Style);
 
             ctl.picFormIcon.Image = ImageTemplate.Logo;
 
@@ -288,9 +301,9 @@ namespace UIX
 
         }
 
-        public UIX.UIXStyle Style { get => _style; }
+        public UIXStyle Style { get => _style; }
 
-        public UIX.UIXImageTemplate ImageTemplate { get => _imageTemplate; }
+        public UIXImageTemplate ImageTemplate { get => _imageTemplate; }
 
     }
 
@@ -301,9 +314,9 @@ namespace UIX
         {
             frm.FormBorderStyle = FormBorderStyle.None;
 
-            UIX.UIXHeader.hdrApply(frm, ctl, style);
+            UIXHeader.hdrApply(frm, ctl, style);
 
-            UIX.UIXHeader.hdrMove(frm, new Control[] { ctl, ctl.lblTitulo, ctl.picFormIcon });
+            UIXHeader.hdrMove(frm, new Control[] { ctl, ctl.lblTitulo, ctl.picFormIcon });
         }
     }
 
@@ -325,7 +338,8 @@ namespace UIX
             //Apply header title style
             ctl.lblTitulo.Location = new Point(style.HeaderButtonSize + (style.HeaderButtonSpace * 2), 12);
             ctl.lblTitulo.BringToFront();
-            UIX.UIXLabel.lblApply(ctl.lblTitulo, style.FormColor, UIX.UIXFont.LATO_BLACK(10f));
+            UIXLabel.lblApply(ctl.lblTitulo, style.FormColor, style.HeaderFont);
+            ctl.lblTitulo.Font = UIXFont.fontSize(ctl.lblTitulo.Font, 10f);
             ctl.lblTitulo.BackColor = style.FormColor.DarkColor;
 
             ctl.picFormIcon.Location = new Point(style.HeaderButtonSpace, style.HeaderButtonSpace);
@@ -343,7 +357,7 @@ namespace UIX
                 ctl.btnSair.Location = new Point(nextButtonX, style.HeaderButtonSpace);
                 ctl.btnSair.BringToFront();
 
-                UIX.UIXButton.btnStyleClose(ctl.btnSair, style.HeaderButtonSize, style.HeaderCloseButtonColor);
+                UIXButton.btnStyleClose(ctl.btnSair, style.HeaderButtonSize, style.HeaderCloseButtonColor);
             }
             //Minimize button
             if (ctl.btnMin != null)
@@ -352,7 +366,7 @@ namespace UIX
                 ctl.btnMin.Location = new Point(nextButtonX, style.HeaderButtonSpace);
                 ctl.btnMin.BringToFront();
 
-                UIX.UIXButton.btnStyleMinimize(ctl.btnMin, style.HeaderButtonSize, style.HeaderButtonColor);
+                UIXButton.btnStyleMinimize(ctl.btnMin, style.HeaderButtonSize, style.HeaderButtonColor);
             }
         }
 
@@ -449,13 +463,13 @@ namespace UIX
 
         public static void btnApply(Button btn, UIXSet colorSet, Font font)
         {
-            UIX.UIXButton.btnApply(btn, colorSet);
+            UIXButton.btnApply(btn, colorSet);
             btn.Font = font;
         }
 
         public static void btnSquare(Button btn, int size)
         {
-            UIX.UIXButton.btnSize(btn, size, size);
+            UIXButton.btnSize(btn, size, size);
         }
 
         public static void btnSize(Button btn, int x, int y)
@@ -466,53 +480,69 @@ namespace UIX
 
         public static void btnStyleClose(Button btn, int size, UIXSet colorSet)
         {
-            UIX.UIXButton.btnApply(btn, colorSet, UIXFont.SANS_SERIF(12));
+            UIXButton.btnApply(btn, colorSet, UIXFont.fontSize(UIXFont.SANS_SERIF, 12F));
             btn.Text = "X";
             btn.TabIndex = int.MaxValue;
-            UIX.UIXButton.btnSquare(btn, size);
+            UIXButton.btnSquare(btn, size);
         }
 
         public static void btnStyleMinimize(Button btn, int size, UIXSet colorSet)
         {
-            UIX.UIXButton.btnApply(btn, colorSet, UIXFont.SANS_SERIF(12));
+            UIXButton.btnApply(btn, colorSet, UIXFont.fontSize(UIXFont.SANS_SERIF, 12F));
             btn.Text = "_";
             btn.TabIndex = int.MaxValue;
-            UIX.UIXButton.btnSquare(btn, size);
+            UIXButton.btnSquare(btn, size);
         }
 
     }
 
     public class UIXCheckBox
     {
-        public static void chkApply(CheckBox box, UIXSet colorSet)
+        public static void chkApply(CheckBox chk, UIXSet colorSet, Font font)
         {
-            box.FlatStyle = FlatStyle.Flat;
-            box.ForeColor = colorSet.ContentColor;
-            box.BackColor = colorSet.DarkColor;
-            box.Font = UIX.UIXFont.LATO_BLACK(box.Font.Size);
+            chk.Font = UIXFont.fontSize(font, chk.Font.Size);
+            UIXCheckBox.chkApply(chk, colorSet);
+        }
+
+        public static void chkApply(CheckBox chk, UIXSet colorSet)
+        {
+            chk.FlatStyle = FlatStyle.Flat;
+            chk.ForeColor = colorSet.ContentColor;
+            chk.BackColor = colorSet.DarkColor;
         }
     }
 
     public class UIXComboBox
     {
-        public static void cboApply(ComboBox box, UIXSet colorSet)
+
+        public static void cboApply(ComboBox cbo, UIXSet colorSet, Font font)
         {
-            box.FlatStyle = FlatStyle.Popup;
-            box.BackColor = colorSet.DarkColor;
-            box.ForeColor = colorSet.ContentColor;
-            box.Margin = new Padding(0);
-            box.Font = UIX.UIXFont.LATO_BLACK(box.Font.Size);
+            cbo.Font = UIXFont.fontSize(font, cbo.Font.Size);
+            UIXComboBox.cboApply(cbo, colorSet);
+        }
+
+        public static void cboApply(ComboBox cbo, UIXSet colorSet)
+        {
+            cbo.FlatStyle = FlatStyle.Popup;
+            cbo.BackColor = colorSet.DarkColor;
+            cbo.ForeColor = colorSet.ContentColor;
+            cbo.Margin = new Padding(0);
         }
     }
 
     public class UIXGroupBox
     {
+        public static void grbApply(GroupBox grb, UIXSet colorSet, Font font)
+        {
+            grb.Font = UIXFont.fontSize(font, grb.Font.Size);
+            grbApply(grb, colorSet);
+        }
+
         public static void grbApply(GroupBox grb, UIXSet colorSet)
         {
             grb.FlatStyle = FlatStyle.Flat;
             grb.BackColor = colorSet.LightColor;
             grb.ForeColor = colorSet.ContentColor;
-            grb.Font = UIX.UIXFont.LATO_BLACK(grb.Font.Size);
         }
     }
 
@@ -520,8 +550,8 @@ namespace UIX
     {
         public static void lblApply(Label lbl, UIXSet colorSet, Font font)
         {
-            lbl.Font = font;
-            UIX.UIXLabel.lblApply(lbl, colorSet);
+            lbl.Font = UIXFont.fontSize(font, lbl.Font.Size);
+            UIXLabel.lblApply(lbl, colorSet);
         }
 
         public static void lblApply(Label lbl, UIXSet colorSet)
@@ -542,6 +572,12 @@ namespace UIX
     public class UIXPanel
     {
 
+        public static void pnlApply(Panel pnl, UIXSet set, Font font)
+        {
+            pnl.Font = UIXFont.fontSize(font, pnl.Font.Size);
+            UIXPanel.pnlApply(pnl, set);
+        }
+
         public static void pnlApply(Panel pnl, UIXSet set)
         {
             pnl.BackColor = set.LightColor;
@@ -552,18 +588,30 @@ namespace UIX
 
     public class UIXTextBox
     {
+
+        public static void txtApply(TextBox txt, UIXSet colorSet, Font font)
+        {
+            txt.Font = UIXFont.fontSize(font, txt.Font.Size);
+            UIXTextBox.txtApply(txt, colorSet);
+        }
+
         public static void txtApply(TextBox txt, UIXSet colorSet)
         {
             txt.BackColor = colorSet.LightColor;
             txt.ForeColor = colorSet.ContentColor;
             txt.BorderStyle = BorderStyle.None;
             txt.Margin = new Padding(5);
-            txt.Font = UIX.UIXFont.LATO_BLACK(txt.Font.Size);
         }
     }
 
     public class UIXUserControl
     {
+
+        public static void uctApply(UserControl uct, UIXSet set, Font font)
+        {
+            uct.Font = UIXFont.fontSize(font, uct.Font.Size);
+            UIXUserControl.uctApply(uct, set);
+        }
 
         public static void uctApply(UserControl uct, UIXSet set)
         {
