404c93d69ac7323db01442173bff79d9f95ad3da
vitorrdgs <rodrigues.victor@outlook.com>
2018-04-28 02:04:09 -0300

    Adicionado uma TextBox com label ao lado.

---
+++ UIX/txtUIX.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace UIX
+{
+    partial class txtUIX
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCampo = new System.Windows.Forms.Label();
+            this.txtCampo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblCampo
+            // 
+            this.lblCampo.AutoSize = true;
+            this.lblCampo.Location = new System.Drawing.Point(1, 1);
+            this.lblCampo.Name = "lblCampo";
+            this.lblCampo.Size = new System.Drawing.Size(43, 13);
+            this.lblCampo.TabIndex = 0;
+            this.lblCampo.Text = "Campo:";
+            this.lblCampo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtCampo
+            // 
+            this.txtCampo.Location = new System.Drawing.Point(50, 1);
+            this.txtCampo.Name = "txtCampo";
+            this.txtCampo.Size = new System.Drawing.Size(99, 20);
+            this.txtCampo.TabIndex = 1;
+            // 
+            // txtUIX
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtCampo);
+            this.Controls.Add(this.lblCampo);
+            this.Name = "txtUIX";
+            this.Size = new System.Drawing.Size(152, 24);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCampo;
+        private System.Windows.Forms.TextBox txtCampo;
+    }
+}
---
+++ UIX/txtUIX.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UIX
+{
+    public partial class txtUIX : UserControl
+    {
+        public txtUIX()
+        {
+            InitializeComponent();
+        }
+
+        public String Campo {
+            get => lblCampo.Text;
+            set {
+                lblCampo.Text = value;
+                int x = lblCampo.Size.Width;
+                txtCampo.Location = new Point(x, txtCampo.Location.Y);
+            }
+        }
+
+        public String Text { get => txtCampo.Text; set => txtCampo.Text = value; }
+
+        public Size Size
+        {
+            get => base.Size;
+            set
+            {
+                Size current = base.Size;
+                txtCampo.Size = new Size(value.Width - lblCampo.Size.Width - 50, txtCampo.Size.Height);
+                base.Size = value;
+            }
+        }
+
+    }
+}
