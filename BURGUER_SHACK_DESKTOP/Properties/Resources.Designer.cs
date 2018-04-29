c430d5817b107f84e10474e012dce4367ea591c5
vitorrdgs <rodrigues.victor@outlook.com>
2018-04-28 22:40:09 -0300

    Adicionado icones para iniciar os trabalhos na MessageBox.

--- BURGUER_SHACK_DESKTOP/Properties/Resources.Designer.cs
+++ BURGUER_SHACK_DESKTOP/Properties/Resources.Designer.cs
@@ -83,6 +83,26 @@ namespace BURGUER_SHACK_DESKTOP.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap confirmar {
+            get {
+                object obj = ResourceManager.GetObject("confirmar", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap erro {
+            get {
+                object obj = ResourceManager.GetObject("erro", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap garcom {
             get {
                 object obj = ResourceManager.GetObject("garcom", resourceCulture);
@@ -126,6 +146,16 @@ namespace BURGUER_SHACK_DESKTOP.Properties {
         internal static System.Drawing.Bitmap hamburger {
             get {
                 object obj = ResourceManager.GetObject("hamburger", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap info {
+            get {
+                object obj = ResourceManager.GetObject("info", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
