fcc4576858f5fa8890479e78597566b369596901
vitorrdgs <rodrigues.victor@outlook.com>
2018-04-29 00:17:26 -0300

    Agora o clnUtil pode aplicar mask no UIX MaskedTextBox.

--- BURGUER_SHACK_DESKTOP/clnUtil.cs
+++ BURGUER_SHACK_DESKTOP/clnUtil.cs
@@ -12,20 +12,43 @@ namespace BURGUER_SHACK_DESKTOP
     class clnUtil
     {
 
+        public static void maskTel(UIX.mtbUIX mtb)
+        {
+            mtb.Mask = "(00) 0000-0000";
+        }
+
+        public static void maskCel(UIX.mtbUIX mtb)
+        {
+            mtb.Mask = "(00) 00000-0000";
+        }
+
+        public static void maskDN(UIX.mtbUIX mtb)
+        {
+            mtb.Mask = "00/00/0000";
+        }
+
+        public static void maskCPF(UIX.mtbUIX mtb)
+        {
+            mtb.Mask = "000,000,000-00";
+        }
+
+        public static void maskRG(UIX.mtbUIX mtb)
+        {
+            mtb.Mask = "00,000,000-0";
+        }
+
         public static void alterarConteudo(Panel pnlConteudo, UserControl uctConteudo, UIX.uctUIX uctUIX, String titulo)
         {
+            if (pnlConteudo.Controls.Count == 1)
+            {
+                pnlConteudo.Controls.Remove(pnlConteudo.Controls[0]);
+            }
 
             clnTemplate.CommonTemplate.uctApply(uctConteudo);
 
             pnlConteudo.Controls.Add(uctConteudo);
             uctConteudo.Location = new Point(0, 0);
             uctConteudo.Size = pnlConteudo.Size;
-
-            if (pnlConteudo.Controls.Count == 2)
-            {
-                pnlConteudo.Controls.Remove(pnlConteudo.Controls[0]);
-            }
-
             if (!pnlConteudo.Visible)
             {
                 pnlConteudo.Visible = true;
