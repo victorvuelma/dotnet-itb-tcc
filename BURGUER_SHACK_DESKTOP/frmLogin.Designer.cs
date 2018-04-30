7799b5f68ad62c52885eca02fb42441c338e98ca
vitorrdgs <rodrigues.victor@outlook.com>
2018-04-30 00:02:46 -0300

    Novo estilo para o formulário de Login.

--- BURGUER_SHACK_DESKTOP/frmLogin.Designer.cs
+++ BURGUER_SHACK_DESKTOP/frmLogin.Designer.cs
@@ -29,44 +29,59 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
-            this.txtNome = new UIX.txtUIX();
-            this.txtSenha = new UIX.txtUIX();
             this.btnEntrar = new System.Windows.Forms.Button();
-            this.uctUIX = new UIX.uctUIX();
             this.picVer = new System.Windows.Forms.PictureBox();
+            this.picUser = new System.Windows.Forms.PictureBox();
+            this.lblProgram = new System.Windows.Forms.Label();
+            this.uctUIX = new UIX.uctUIX();
+            this.txtSenha = new UIX.txtUIX();
+            this.txtNome = new UIX.txtUIX();
             ((System.ComponentModel.ISupportInitialize)(this.picVer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUser)).BeginInit();
             this.SuspendLayout();
-            // 
-            // txtNome
-            // 
-            this.txtNome.AccessibleName = "Nome";
-            this.txtNome.Campo = "Nome";
-            this.txtNome.Location = new System.Drawing.Point(17, 88);
-            this.txtNome.MaxLength = 32767;
-            this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(271, 20);
-            this.txtNome.TabIndex = 7;
-            // 
-            // txtSenha
-            // 
-            this.txtSenha.AccessibleName = "Senha";
-            this.txtSenha.Campo = "Senha";
-            this.txtSenha.Location = new System.Drawing.Point(13, 114);
-            this.txtSenha.MaxLength = 32767;
-            this.txtSenha.Name = "txtSenha";
-            this.txtSenha.Size = new System.Drawing.Size(275, 20);
-            this.txtSenha.TabIndex = 8;
             // 
             // btnEntrar
             // 
-            this.btnEntrar.Location = new System.Drawing.Point(192, 156);
+            this.btnEntrar.Location = new System.Drawing.Point(364, 199);
             this.btnEntrar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnEntrar.Name = "btnEntrar";
             this.btnEntrar.Size = new System.Drawing.Size(96, 31);
             this.btnEntrar.TabIndex = 9;
-            this.btnEntrar.Text = "ENTRAR";
+            this.btnEntrar.Text = "ACESSAR";
             this.btnEntrar.UseVisualStyleBackColor = true;
             this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
+            // 
+            // picVer
+            // 
+            this.picVer.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picVer.Image = global::BURGUER_SHACK_DESKTOP.Properties.Resources.eye;
+            this.picVer.Location = new System.Drawing.Point(465, 140);
+            this.picVer.Name = "picVer";
+            this.picVer.Size = new System.Drawing.Size(20, 20);
+            this.picVer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picVer.TabIndex = 12;
+            this.picVer.TabStop = false;
+            this.picVer.MouseEnter += new System.EventHandler(this.picVer_MouseEnter);
+            this.picVer.MouseLeave += new System.EventHandler(this.picVer_MouseLeave);
+            // 
+            // picUser
+            // 
+            this.picUser.Image = global::BURGUER_SHACK_DESKTOP.Properties.Resources.usuarios;
+            this.picUser.Location = new System.Drawing.Point(30, 80);
+            this.picUser.Name = "picUser";
+            this.picUser.Size = new System.Drawing.Size(150, 150);
+            this.picUser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picUser.TabIndex = 13;
+            this.picUser.TabStop = false;
+            // 
+            // lblProgram
+            // 
+            this.lblProgram.Location = new System.Drawing.Point(0, 280);
+            this.lblProgram.Name = "lblProgram";
+            this.lblProgram.Size = new System.Drawing.Size(500, 20);
+            this.lblProgram.TabIndex = 14;
+            this.lblProgram.Text = "BurguerShack :: Versão 1.0 :: Copyright 2018";
+            this.lblProgram.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // uctUIX
             // 
@@ -74,32 +89,43 @@
             this.uctUIX.Location = new System.Drawing.Point(0, 0);
             this.uctUIX.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.uctUIX.Name = "uctUIX";
-            this.uctUIX.Size = new System.Drawing.Size(319, 43);
+            this.uctUIX.Size = new System.Drawing.Size(500, 40);
             this.uctUIX.TabIndex = 11;
             this.uctUIX.UIXButtonCloseEnabled = true;
             this.uctUIX.UIXButtonMinEnabled = false;
             this.uctUIX.UIXImage = global::BURGUER_SHACK_DESKTOP.Properties.Resources.usuarios;
-            this.uctUIX.UIXTitle = "BurgerShack :: Entrar";
+            this.uctUIX.UIXTitle = "BurgerShack :: Acessar o Sistema";
             this.uctUIX.Close += new System.EventHandler(this.uctUIX_Close);
             // 
-            // picVer
+            // txtSenha
             // 
-            this.picVer.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.picVer.Image = global::BURGUER_SHACK_DESKTOP.Properties.Resources.eye;
-            this.picVer.Location = new System.Drawing.Point(294, 107);
-            this.picVer.Name = "picVer";
-            this.picVer.Size = new System.Drawing.Size(27, 25);
-            this.picVer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.picVer.TabIndex = 12;
-            this.picVer.TabStop = false;
-            this.picVer.MouseEnter += new System.EventHandler(this.picVer_MouseEnter);
-            this.picVer.MouseLeave += new System.EventHandler(this.picVer_MouseLeave);
+            this.txtSenha.AccessibleName = "Senha";
+            this.txtSenha.Campo = "Senha";
+            this.txtSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(190, 145);
+            this.txtSenha.MaxLength = 32767;
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(270, 30);
+            this.txtSenha.TabIndex = 8;
+            // 
+            // txtNome
+            // 
+            this.txtNome.AccessibleName = "Nome";
+            this.txtNome.Campo = "Nome";
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(190, 110);
+            this.txtNome.MaxLength = 32767;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(270, 30);
+            this.txtNome.TabIndex = 7;
             // 
             // frmLogin
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(326, 200);
+            this.ClientSize = new System.Drawing.Size(500, 300);
+            this.Controls.Add(this.lblProgram);
+            this.Controls.Add(this.picUser);
             this.Controls.Add(this.picVer);
             this.Controls.Add(this.uctUIX);
             this.Controls.Add(this.btnEntrar);
@@ -113,6 +139,7 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.frmLogin_Load);
             ((System.ComponentModel.ISupportInitialize)(this.picVer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUser)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -123,6 +150,8 @@
         private System.Windows.Forms.Button btnEntrar;
         private UIX.uctUIX uctUIX;
         private System.Windows.Forms.PictureBox picVer;
+        private System.Windows.Forms.PictureBox picUser;
+        private System.Windows.Forms.Label lblProgram;
     }
 }
 
