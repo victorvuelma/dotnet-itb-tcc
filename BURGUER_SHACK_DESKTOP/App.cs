fdbd7c2d4d14e259a99102d59e9770fb6a0bb505
vitorrdgs <rodrigues.victor@outlook.com>
2018-06-18 14:59:31 -0300

    Adicionado callback para erros do banco de dados.

--- BURGUER_SHACK_DESKTOP/App.cs
+++ BURGUER_SHACK_DESKTOP/App.cs
@@ -7,28 +7,35 @@ using System.Threading.Tasks;
 using System.Drawing;
 using UIX;
 using SQL_POWERUP;
+using System.Windows.Forms;
 
 namespace BURGUER_SHACK_DESKTOP
 {
     class App
     {
 
-        private static sqlDatabase _appDatabase = new sqlDatabase {
+        private static sqlDatabase _appDatabase = new sqlDatabase
+        {
             Database = AppDb.DATABASE,
             User = AppDb.USER,
             Pass = AppDb.PASS,
-            Source = AppDb.SOURCE
+            Source = AppDb.SOURCE,
+            ExceptionCall = delegate (Exception ex) {
+                clnUtilMensagem.mostrarOk("Banco de Dados", "Um erro ocorreu ao tentar processar a requisição\n" + ex.Message, clnUtilMensagem.MensagemIcone.ERRO);
+                Application.Restart();
+            }
         };
 
         private static String _appName = "Burguer Shack";
 
         private static UIX.uixStyle _appVisualStyle = UIX.uixStyle.BLUE;
-        private static UIX.uixTemplate _appVisualTemplate = new UIX.uixTemplate {
+        private static UIX.uixTemplate _appVisualTemplate = new UIX.uixTemplate
+        {
             Style = AppVisualStyle,
             Icon = Properties.Resources.burguershack_ico
         };
 
-        public static sqlDatabase AppDatabase { get => _appDatabase;}
+        public static sqlDatabase AppDatabase { get => _appDatabase; }
         public static String AppName { get => _appName; }
 
         public static UIX.uixTemplate AppVisualTemplate { get => _appVisualTemplate; }
--- SQL_POWERUP/sqlDatabase.cs
+++ SQL_POWERUP/sqlDatabase.cs
@@ -12,24 +12,34 @@ namespace SQL_POWERUP
     public class sqlDatabase
     {
 
-        private string database;
-        private string user;
-        private string pass;
+        private Action<Exception> _exceptionCall;
 
-        private string source;
+        private string _database;
+        private string _user;
+        private string _pass;
+        private string _source;
 
-        public string Database { get => database; set => database = value; }
-        public string User { get => user; set => user = value; }
-        public string Pass { get => pass; set => pass = value; }
-        public string Source { get => source; set => source = value; }
+        public Action<Exception> ExceptionCall { get => _exceptionCall; set => _exceptionCall = value; }
+        public string Database { get => _database; set => _database = value; }
+        public string User { get => _user; set => _user = value; }
+        public string Pass { get => _pass; set => _pass = value; }
+        public string Source { get => _source; set => _source = value; }
 
         public SqlConnection connect()
         {
-            string connStr = "Data Source=" + Source + ";Initial Catalog=" + Database + ";User Id=" + User + ";Password=" + Pass;
+            try
+            {
+                string connStr = "Data Source=" + Source + ";Initial Catalog=" + Database + ";User Id=" + User + ";Password=" + Pass;
 
-            SqlConnection conn = new SqlConnection(connStr);
-            conn.Open();
-            return conn;
+                SqlConnection conn = new SqlConnection(connStr);
+                conn.Open();
+                return conn;
+            }
+            catch (Exception ex)
+            {
+                ExceptionCall.Invoke(ex);
+            }
+            return null;
         }
 
         public static void close(SqlConnection conn)
@@ -51,10 +61,15 @@ namespace SQL_POWERUP
                 adapter.Fill(table);
                 return table;
             }
+            catch (Exception ex)
+            {
+                ExceptionCall.Invoke(ex);
+            }
             finally
             {
                 close(command.Connection);
             }
+            return null;
         }
 
         public SqlDataReader returnReader(SqlCommand command) => command.ExecuteReader(CommandBehavior.CloseConnection);
@@ -65,10 +80,15 @@ namespace SQL_POWERUP
             {
                 return command.ExecuteNonQuery();
             }
+            catch (Exception ex)
+            {
+                ExceptionCall.Invoke(ex);
+            }
             finally
             {
                 close(command.Connection);
             }
+            return 0;
         }
 
         public object executeScalar(SqlCommand command)
@@ -77,10 +97,15 @@ namespace SQL_POWERUP
             {
                 return command.ExecuteScalar();
             }
+            catch (Exception ex)
+            {
+                ExceptionCall.Invoke(ex);
+            }
             finally
             {
                 close(command.Connection);
             }
+            return null;
         }
 
     }
