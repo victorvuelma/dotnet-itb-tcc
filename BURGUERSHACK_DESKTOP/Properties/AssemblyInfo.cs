dc91e0d91aea993766c65698c4ac649757e64755
vitorrdgs <rodrigues.victor@outlook.com>
2018-07-01 20:13:27 -0300

    Correção de conflitos com as alterações de nome dos projetos.

--- BURGUERSHACK_DESKTOP/Properties/AssemblyInfo.cs
+++ BURGUERSHACK_DESKTOP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BURGUER_SHACK_DESKTOP")]
+[assembly: AssemblyTitle("BURGUERSHACK_DESKTOP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BURGUER_SHACK_DESKTOP")]
+[assembly: AssemblyProduct("BURGUERSHACK_DESKTOP")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
