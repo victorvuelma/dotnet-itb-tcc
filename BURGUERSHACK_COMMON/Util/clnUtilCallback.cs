e4808c890f6314174abf15b161d3b3f331aa9d3d
vitorrdgs <rodrigues.victor@outlook.com>
2018-07-04 00:19:47 -0300

    Continuando com o processo de separacao, UtilCallback agora faz parte do Common.

---
+++ BURGUERSHACK_COMMON/Util/clnUtilCallback.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BURGUERSHACK_COMMON
+{
+
+    public interface clnUtilResponseCallback<R>
+    {
+        R call();
+    }
+
+
+    public interface clnUtilResponseCallback<R, X, Y>
+    {
+        R call(X x, Y y);
+    }
+
+    public interface clnUtilResponseCallback<R, X>
+    {
+        R call(X x);
+    }
+
+    public interface clnUtilCallback : clnUtilResponseCallback<bool> { };
+
+    public interface clnUtilCallback<X> : clnUtilResponseCallback<bool, X> { };
+
+    public interface clnUtilCallback<X, Y> : clnUtilResponseCallback<bool, X, Y> { };
+
+}
--- BURGUERSHACK_DESKTOP/clnUtilVisualizar.cs
+++ BURGUERSHACK_DESKTOP/clnUtilVisualizar.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 using System.Windows.Forms;
+using BURGUERSHACK_COMMON;
 
 namespace BURGUERSHACK_DESKTOP
 {
