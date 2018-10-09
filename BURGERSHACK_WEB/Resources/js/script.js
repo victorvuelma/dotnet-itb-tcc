54854486e4d91ce8817166507223488ef3bcded2
vitorrdgs <rodrigues.victor@outlook.com>
2018-10-09 16:06:52 -0300

    Finalizado o layout do web.

---
+++ BURGERSHACK_WEB/Resources/js/script.js
@@ -0,0 +1,32 @@
+﻿/*
+ * Retirado de
+ * https://stackoverflow.com/questions/16260498/push-footer-to-bottom-when-page-is-not-full
+ * 
+ * Modificacao do calculo da margin.
+ */
+
+
+// function to set the height on fly
+function autoHeight() {
+    var docHeight = $(document).outerHeight(true);
+    var footerPos = $('body').outerHeight(true);
+
+    if (docHeight > footerPos) {
+
+        var margin =  docHeight - footerPos + 25;
+
+        $('footer').css({
+            marginTop: margin
+        });
+    }
+}
+
+// onDocumentReady function bind
+$(document).ready(function () {
+    autoHeight();
+});
+
+// onResize bind of the function
+$(window).on('resize', function () {
+    autoHeight();
+});
